8e819df0837a17dd804c4d98230c8f5b5a99cdbc
ReaganNet <146112059+ReaganNet@users.noreply.github.com>
2024-02-21 14:57:40 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,36 @@
+<?php
+// Initialize the session
+session_start();
+
+// Check if the user is logged in, if not then redirect to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-6 offset-md-3">
+                <div class="page-header">
+                    <h1>Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to our site.</h1>
+                </div>
+                <p>
+                    <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
+                    <a href="logout.php" class="btn btn-danger ml-3">Sign Out of Your Account</a>
+                </p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
 
